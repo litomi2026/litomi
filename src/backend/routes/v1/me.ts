0efff43e302096ef646494428db90c6b7d99fd83
litomi2026 <litomi2026@gmail.com>
2025-10-22 22:49:31 +0900

    feat: 내 정보 불러오기

--- src/backend/routes/v1/me.ts
+++ src/backend/routes/v1/me.ts
@@ -1,7 +1,13 @@
+import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
+import { deleteCookie } from 'hono/cookie'
 import { HTTPException } from 'hono/http-exception'
 
 import { getUserId } from '@/backend/utils/auth'
+import { CookieKey } from '@/constants/storage'
+import { createCacheControl } from '@/crawler/proxy-utils'
+import { db } from '@/database/supabase/drizzle'
+import { userTable } from '@/database/supabase/schema'
 
 import type { Env } from '../..'
 
@@ -14,7 +20,29 @@ meRoutes.get('/', async (c) => {
     throw new HTTPException(401)
   }
 
-  return c.json({ id: userId })
+  const [user] = await db
+    .select({
+      id: userTable.id,
+      loginId: userTable.loginId,
+      name: userTable.name,
+      nickname: userTable.nickname,
+      imageURL: userTable.imageURL,
+    })
+    .from(userTable)
+    .where(eq(userTable.id, userId))
+
+  if (!user) {
+    deleteCookie(c, CookieKey.ACCESS_TOKEN)
+    deleteCookie(c, CookieKey.REFRESH_TOKEN)
+    throw new HTTPException(404)
+  }
+
+  const cacheControl = createCacheControl({
+    private: true,
+    maxAge: 3,
+  })
+
+  return c.json(user, { headers: { 'Cache-Control': cacheControl } })
 })
 
 export default meRoutes
