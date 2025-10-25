b459004bbe1096e0a04a97b34fd361cde631e0c9
litomi2026 <litomi2026@gmail.com>
2025-10-25 18:37:50 +0900

    fix: #336

--- src/app/(navigation)/search/SearchResults.tsx
+++ src/app/(navigation)/search/SearchResults.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo } from 'react'
+import { Fragment, useMemo } from 'react'
 
 import { useSearchQuery } from '@/app/(navigation)/search/useSearchQuery'
 import { Sort } from '@/app/api/proxy/k/search/schema'
@@ -11,6 +11,8 @@ import { ViewCookie } from '@/utils/param'
 import { MANGA_LIST_GRID_COLUMNS } from '@/utils/style'
 
 import RandomRefreshButton from '../(top-navigation)/RandomRefreshButton'
+
+const DONATION_CARD_INTERVAL = 30
 
 type Props = {
   view: ViewCookie
@@ -29,10 +31,9 @@ export default function SearchResults({ view, sort }: Props) {
   })
 
   if (isLoading) {
-    const skeletonCount = view === ViewCookie.IMAGE ? 11 : 5
+    const skeletonCount = view === ViewCookie.IMAGE ? 12 : 6
     return (
       <ul className={`grid ${MANGA_LIST_GRID_COLUMNS[view]} gap-2 grow`}>
-        <MangaCardDonation />
         {Array.from({ length: skeletonCount }).map((_, i) => (
           <MangaCardSkeleton key={i} />
         ))}
@@ -61,10 +62,13 @@ export default function SearchResults({ view, sort }: Props) {
               />
             </li>
           ) : (
-            <MangaCard index={i} key={manga.id} manga={manga} showSearchFromNextButton />
+            <Fragment key={manga.id}>
+              <MangaCard index={i} manga={manga} showSearchFromNextButton />
+              {i % DONATION_CARD_INTERVAL === DONATION_CARD_INTERVAL - 1 && <MangaCardDonation />}
+            </Fragment>
           ),
         )}
-        {isFetchingNextPage && <MangaCardDonation />}
+        {isFetchingNextPage && <MangaCardSkeleton />}
       </ul>
       {isRandomSort ? (
         <RandomRefreshButton
