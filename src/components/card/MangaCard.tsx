621123cd0cd7587e511a0d7b90315d5c8e35147a
litomi2026 <litomi2026@gmail.com>
2025-10-25 18:37:19 +0900

    feat: tailwind-merge 도입

--- src/components/card/MangaCard.tsx
+++ src/components/card/MangaCard.tsx
@@ -1,6 +1,7 @@
 import { ErrorBoundary } from '@suspensive/react'
 import { ExternalLink } from 'lucide-react'
 import { memo, ReactNode, Suspense } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 import { Manga } from '@/types/manga'
 import { getViewerLink } from '@/utils/manga'
@@ -31,7 +32,10 @@ export default memo(MangaCard)
 export function MangaCardSkeleton({ children, className = '' }: { children?: ReactNode; className?: string }) {
   return (
     <li
-      className={`animate-fade-in rounded-xl bg-zinc-900 border-2 aspect-[3/4] w-full h-full flex flex-col justify-center items-center gap-1 ${className}`}
+      className={twMerge(
+        'animate-fade-in rounded-xl bg-zinc-900 border-2 aspect-[3/4] w-full h-full flex flex-col justify-center items-center gap-1',
+        className,
+      )}
     >
       {children}
     </li>
