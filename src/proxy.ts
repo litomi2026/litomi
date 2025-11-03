b8b6db1bdddc9c58d98b51adc0930017d8d814d8
litomi2026 <litomi2026@gmail.com>
2025-11-03 15:12:46 +0900

    refactor: middleware -> proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,29 @@
-import { RequestCookies, ResponseCookies } from 'next/dist/server/web/spec-extension/cookies'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { CookieKey } from './constants/storage'
 import { getAccessTokenCookieConfig } from './utils/cookie'
 import { JWTType, verifyJWT } from './utils/jwt'
 
-export async function middleware({ nextUrl, method, cookies, headers }: NextRequest) {
+export const config = {
+  // DOCS: The matcher values need to be constants so they can be statically analyzed at build-time
+  // https://clerk.com/blog/skip-nextjs-middleware-static-and-public-files
+  // DOCS: Ignoring matching prefetches
+  // https://nextjs.org/docs/app/guides/content-security-policy#adding-a-nonce-with-middleware
+  matcher: [
+    {
+      source: '/((?!.*\\.|_next/static|_next/image).*)',
+      has: [{ type: 'cookie', key: 'rt' }],
+      missing: [{ type: 'header', key: 'next-router-prefetch' }],
+    },
+    {
+      source: '/((?!.*\\.|_next/static|_next/image).*)',
+      has: [{ type: 'cookie', key: 'at' }],
+      missing: [{ type: 'header', key: 'next-router-prefetch' }],
+    },
+  ],
+}
+
+export async function proxy({ nextUrl, method, cookies }: NextRequest) {
   const { pathname } = nextUrl
 
   if (
@@ -50,40 +68,5 @@ export async function middleware({ nextUrl, method, cookies, headers }: NextRequ
   const response = NextResponse.next()
   const { key, value, options } = await getAccessTokenCookieConfig(userId)
   response.cookies.set(key, value, options)
-  setCookieToRequest(headers, response)
   return response
-}
-
-// https://github.com/vercel/next.js/discussions/50374
-function setCookieToRequest(requestHeaders: Headers, res: NextResponse) {
-  const setCookies = new ResponseCookies(res.headers)
-  const newReqHeaders = new Headers(requestHeaders)
-  const newReqCookies = new RequestCookies(newReqHeaders)
-  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie))
-  const dummyRes = NextResponse.next({ request: { headers: newReqHeaders } })
-
-  for (const [key, value] of dummyRes.headers) {
-    if (key === 'x-middleware-override-headers' || key.startsWith('x-middleware-request-')) {
-      res.headers.set(key, value)
-    }
-  }
-}
-
-export const config = {
-  // DOCS: The matcher values need to be constants so they can be statically analyzed at build-time
-  // https://clerk.com/blog/skip-nextjs-middleware-static-and-public-files
-  // DOCS: Ignoring matching prefetches
-  // https://nextjs.org/docs/app/guides/content-security-policy#adding-a-nonce-with-middleware
-  matcher: [
-    {
-      source: '/((?!.*\\.|_next/static|_next/image).*)',
-      has: [{ type: 'cookie', key: 'rt' }],
-      missing: [{ type: 'header', key: 'next-router-prefetch' }],
-    },
-    {
-      source: '/((?!.*\\.|_next/static|_next/image).*)',
-      has: [{ type: 'cookie', key: 'at' }],
-      missing: [{ type: 'header', key: 'next-router-prefetch' }],
-    },
-  ],
 }
