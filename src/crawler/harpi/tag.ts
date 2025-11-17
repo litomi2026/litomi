72e97b56617c1933e7702a2b80d72b7b15887ca3
litomi2026 <litomi2026@gmail.com>
2025-11-17 09:47:18 +0900

    chore: 태그 업데이트
    chore: 태그 업데이트
    
    Update tag-unisex.json

--- src/crawler/harpi/tag.ts
+++ src/crawler/harpi/tag.ts
@@ -5,7 +5,27 @@ type HarpiTag = {
   ko?: string
 }
 
+/**
+ * HARPI_TAG_MAP is a map of Harpi tag IDs to their English and Korean names.
+ *
+```js
+fetch('https://harpi.in/animation/attributes')
+  .then((response) => response.json())
+  .then((data) => {
+    const tagsMap = Object.fromEntries(
+      data.data.tags.map((tag) => [
+        tag.id,
+        { ko: tag.korStr, en: tag.engStr.replaceAll(' ', '_') },
+      ]),
+    )
+    const sortedTagsMap = Object.keys(tagsMap)
+      .sort()
+      .reduce((newObj, key) => {
+        newObj[key] = tagsMap[key]
+        return newObj
+      }, {})
+    console.log(JSON.stringify(sortedTagsMap, null, 2))
+  })
+```
+ */
 export const HARPI_TAG_MAP: Record<string, HarpiTag> = harpiTagJSON
-
-// const a = [{ ... }]
-// Object.fromEntries(a.map(aa => [aa.id, { ko: aa.korStr, en: aa.engStr.replaceAll(' ', '_'),  }]))
