57961928a7c58fcb6fc3674990b7fd631d3ac786
litomi2026 <litomi2026@gmail.com>
2026-01-29 01:26:30 +0800

    code 정리

--- src/database/neon/drizzle.ts
+++ src/database/neon/drizzle.ts
@@ -1,20 +1,20 @@
-import { neon, neonConfig } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
-import 'server-only'
-import ws from 'ws'
+// import { neon, neonConfig } from '@neondatabase/serverless'
+// import { drizzle } from 'drizzle-orm/neon-http'
+// import 'server-only'
+// import ws from 'ws'
 
-import { env } from '@/env/server.next'
+// import { env } from '@/env/server.next'
 
-import * as schema from './schema'
+// import * as schema from './schema'
 
-const { NEON_DATABASE_URL, NEON_DATABASE_URL_RO } = env
+// const { NEON_DATABASE_URL, NEON_DATABASE_URL_RO } = env
 
-if (!NEON_DATABASE_URL || !NEON_DATABASE_URL_RO) {
-  throw new Error('NEON_DATABASE_URL and NEON_DATABASE_URL_RO are required to use Neon DB')
-}
+// if (!NEON_DATABASE_URL || !NEON_DATABASE_URL_RO) {
+//   throw new Error('NEON_DATABASE_URL and NEON_DATABASE_URL_RO are required to use Neon DB')
+// }
 
-neonConfig.webSocketConstructor = ws
-const neonROClient = neon(NEON_DATABASE_URL_RO)
-const neonClient = neon(NEON_DATABASE_URL)
-export const neonDBRO = drizzle({ client: neonROClient, schema })
-export const neonDB = drizzle({ client: neonClient, schema })
+// neonConfig.webSocketConstructor = ws
+// const neonROClient = neon(NEON_DATABASE_URL_RO)
+// const neonClient = neon(NEON_DATABASE_URL)
+// export const neonDBRO = drizzle({ client: neonROClient, schema })
+// export const neonDB = drizzle({ client: neonClient, schema })
