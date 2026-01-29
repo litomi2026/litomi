49b9f5f12ded3157fc1cc4339c9b2c68a61bf413
litomi2026 <litomi2026@gmail.com>
2026-01-29 11:23:45 +0800

    fix: rate

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.05,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
