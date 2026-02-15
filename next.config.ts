eb672b41a1a480fa5044efac5852a9bd89f4d33f
litomi2026 <litomi2026@gmail.com>
2026-02-15 18:11:00 +0900

    fix: cacheHandler 삭제

--- next.config.ts
+++ next.config.ts
@@ -97,8 +97,6 @@ const nextConfig: NextConfig = {
   ...(process.env.BUILD_OUTPUT === 'standalone' && {
     output: 'standalone',
     transpilePackages: ['@t3-oss/env-nextjs', '@t3-oss/env-core'],
-    // cacheHandler: require.resolve('./cache-handler.mjs'),
-    // cacheMaxMemorySize: 0,
   }),
 }
 
