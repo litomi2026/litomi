119829f5663f499642541dc9e75abca913984a5a
곽태욱 <gwak2837@gmail.com>
2026-03-16 00:16:03 +0900

    Revert "fix: action 삭제"
    This reverts commit 7ef0c8f2a3bb61de6e80c98af272f4bf14fd990c.

--- src/components/ReadingHistoryWarmup.tsx
+++ src/components/ReadingHistoryWarmup.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import ms from 'ms'
 import { useEffect } from 'react'
 
-import type { GETV1ReadingHistoryResponse } from '@/backend/api/v1/library/history/GET'
+import type { GETV1ReadingHistoryResponse } from '@/backend/api/v1/library/history'
 
 import { POINT_CONSTANTS } from '@/constants/points'
 import { QueryKeys } from '@/constants/query'
