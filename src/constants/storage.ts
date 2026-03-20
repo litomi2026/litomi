3adb0ee3944c78e315290c9eb4119c0a63d86c49
litomi2026 <litomi2026@gmail.com>
2026-03-20 23:28:49 +0900

    fix: 문구 수정

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -11,8 +11,8 @@ export enum CookieKey {
 export enum LocalStorageKey {
   // zustand
   CONTROLLER_SCREEN_FIT = 'controller/screen-fit',
-  CONTROLLER_NAVIGATION_MODE = 'controller/navigation-mode',
-  CONTROLLER_TOUCH_ORIENTATION = 'controller/touch-orientation',
+  CONTROLLER_VIEWER_MODE = 'controller/viewer-mode',
+  CONTROLLER_ORIENTATION = 'controller/orientation',
   CONTROLLER_PAGE_VIEW = 'controller/page-view',
   CONTROLLER_IMAGE_WIDTH = 'controller/image-width',
   CONTROLLER_READING_DIRECTION = 'controller/reading-direction',
