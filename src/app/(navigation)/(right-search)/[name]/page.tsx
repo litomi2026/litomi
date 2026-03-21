fcb8d445663bd395fa6e75cf6fd702942a3724f7
litomi2026 <litomi2026@gmail.com>
2026-03-22 00:56:56 +0900

    fix: 문구, meta

--- src/app/(navigation)/(right-search)/[name]/page.tsx
+++ src/app/(navigation)/(right-search)/[name]/page.tsx
@@ -1,7 +1,19 @@
+import { Metadata } from 'next'
+
+import { defaultOpenGraph, SHORT_NAME } from '@/constants'
 import { getUsernameFromParam } from '@/utils/param'
 
 import { getUserByName } from './common'
 import UserPostList from './UserPostList'
+
+export const metadata: Metadata = {
+  title: '내 이야기',
+  openGraph: {
+    ...defaultOpenGraph,
+    title: `내 이야기 - ${SHORT_NAME}`,
+    url: '/@/settings',
+  },
+}
 
 export default async function Page({ params }: PageProps<'/[name]'>) {
   const { name } = await params
