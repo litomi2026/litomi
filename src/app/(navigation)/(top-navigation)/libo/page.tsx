4a23fcd96b04912c075f08970fbf1f52676a0ed6
곽태욱 <gwak2837@gmail.com>
2026-03-22 00:56:56 +0900

    fix: 문구, meta

--- src/app/(navigation)/(top-navigation)/libo/page.tsx
+++ src/app/(navigation)/(top-navigation)/libo/page.tsx
@@ -1,6 +1,21 @@
+import { Metadata } from 'next'
+
 import JuicyAdsScript from '@/components/ads/juicy-ads/JuicyAdsScript'
+import { generateOpenGraphMetadata } from '@/constants'
 
 import RewardedAdSection from './RewardedAdSection'
+
+export const metadata: Metadata = {
+  title: '리보',
+  ...generateOpenGraphMetadata({
+    title: '리보',
+    url: '/libo',
+  }),
+  alternates: {
+    canonical: '/libo',
+    languages: { ko: '/libo' },
+  },
+}
 
 export default function PointsPage() {
   return (
