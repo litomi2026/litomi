1b3b59a1af38a8bd8ffe0311262d28311da3afd3
litomi2026 <litomi2026@gmail.com>
2026-03-31 22:31:39 +0900

    fix: 컴포넌트 정리, 그림자 정리

--- src/app/(navigation)/search/page.tsx
+++ src/app/(navigation)/search/page.tsx
@@ -1,9 +1,13 @@
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
+import NonAdultJuicyAdsBanner from '@/components/ads/juicy-ads/NonAdultJuicyAdsBanner'
 import { generateOpenGraphMetadata } from '@/constants'
 import { getViewFromSearchParams } from '@/utils/param'
 
-import SearchPage from './SearchPage'
+import ActiveFilters, { ClearAllFilters } from './ActiveFilters'
+import SearchResult, { SearchResultLoading } from './SearchResults'
+import TrendingKeywords from './TrendingKeywords'
 
 export const metadata: Metadata = {
   title: '검색',
@@ -44,8 +48,27 @@ export default async function Page({ searchParams }: PageProps<'/search'>) {
   }
 
   const view = getViewFromSearchParams(params)
+  const hasActiveFilters = Boolean(Object.values(filters).some(Boolean))
 
-  return <SearchPage filters={filters} hasActiveFilters={Boolean(Object.values(filters).some(Boolean))} view={view} />
+  return (
+    <>
+      {hasActiveFilters ? (
+        <div className="gap-2 pb-2 hidden sm:grid">
+          <div className="flex items-center justify-between">
+            <h3 className="text-sm font-medium text-zinc-400">적용된 조건</h3>
+            <ClearAllFilters />
+          </div>
+          <ActiveFilters filters={filters} />
+        </div>
+      ) : (
+        <TrendingKeywords view={view} />
+      )}
+      <NonAdultJuicyAdsBanner />
+      <Suspense fallback={<SearchResultLoading view={view} />}>
+        <SearchResult />
+      </Suspense>
+    </>
+  )
 }
 
 function getSearchParamValue(value: string | string[] | undefined) {
