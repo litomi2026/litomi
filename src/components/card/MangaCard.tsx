159e26be87a03d52c6274ad00cd64ca7bbe96338
litomi2026 <litomi2026@gmail.com>
2026-04-15 09:46:46 +0900

    fix: content-auto 삭제

--- src/components/card/MangaCard.tsx
+++ src/components/card/MangaCard.tsx
@@ -39,11 +39,12 @@ type SkeletonProps = {
 
 const VARIANT_CONFIG = {
   [View.CARD]: {
-    containerClassName: 'flex flex-col content-auto',
+    // NOTE: iOS Safari 이슈로 content-auto 비활성화 (크기 계산이 생략되어 스크롤 시 CLS 발생)
+    containerClassName: 'flex flex-col',
     showBody: true,
   },
   [View.IMAGE]: {
-    // NOTE: iOS Safari 이슈로 View.IMAGE 일 때 content-auto 비활성화
+    // NOTE: iOS Safari 이슈로 content-auto 비활성화 (초기 크기가 이상함)
     containerClassName: '',
     showBody: false,
   },
