4650ab09e0835af906dc92d142fc51585a81885c
litomi2026 <litomi2026@gmail.com>
2026-04-15 22:57:49 +0900

    feat: 정보 변경 API

--- src/backend/api/v1/me/index.ts
+++ src/backend/api/v1/me/index.ts
@@ -5,6 +5,7 @@ import { requireAuth } from '@/backend/middleware/require-auth'
 
 import deleteRoute from './DELETE'
 import getRoute from './GET'
+import patchRoute from './PATCH'
 import sessionRoutes from './session'
 import settingsRoutes from './settings'
 
@@ -12,10 +13,9 @@ const route = new Hono<Env>()
 
 route.use('*', requireAuth)
 route.route('/', getRoute)
+route.route('/', patchRoute)
 route.route('/', deleteRoute)
 route.route('/session', sessionRoutes)
 route.route('/settings', settingsRoutes)
-
-export type { DELETEV1MeBody, DELETEV1MeResponse } from './DELETE'
 
 export default route
