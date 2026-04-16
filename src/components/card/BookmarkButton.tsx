102c23e40c20bb9a60cf452100a13acf35a1d966
litomi2026 <litomi2026@gmail.com>
2026-04-16 14:35:16 +0900

    Update BookmarkButton.tsx

--- src/components/card/BookmarkButton.tsx
+++ src/components/card/BookmarkButton.tsx
@@ -70,8 +70,6 @@ export default function BookmarkButton({ manga, className }: Props) {
           duration: ms('5 seconds'),
           id: toastId,
         })
-      } else {
-        toast.success('북마크에서 삭제했어요', { action: null, id: toastId })
       }
 
       queryClient.setQueryData<GETV1BookmarkIdResponse>(QueryKeys.bookmarks, (oldBookmarks) => {
