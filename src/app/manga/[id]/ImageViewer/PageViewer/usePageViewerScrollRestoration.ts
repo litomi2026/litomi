6d421afc7effc6f93f5e61f7e0d21790b3a7112c
litomi2026 <litomi2026@gmail.com>
2026-05-01 19:56:59 +0900

    Create usePageViewerScrollRestoration.ts

---
+++ src/app/manga/[id]/ImageViewer/PageViewer/usePageViewerScrollRestoration.ts
@@ -0,0 +1,53 @@
+import { type RefObject, useEffect, useRef } from 'react'
+
+import { useImageIndexStore } from '../store/imageIndex'
+import { useOrientationStore } from '../store/orientation'
+
+type Params = {
+  scrollRef: RefObject<HTMLDivElement | null>
+}
+
+export default function usePageViewerScrollRestoration({ scrollRef }: Params) {
+  const getOrientation = useOrientationStore((state) => state.getOrientation)
+  const currentIndex = useImageIndexStore((state) => state.imageIndex)
+  const previousIndexRef = useRef(currentIndex)
+
+  // NOTE: 이미지 스크롤 가능할 때 페이지 변경 시 스크롤 위치를 자연스럽게 설정함
+  useEffect(() => {
+    const scroll = scrollRef.current
+    if (!scroll) {
+      return
+    }
+
+    const isVerticallyScrollable = scroll.scrollHeight > scroll.clientHeight
+    const isHorizontallyScrollable = scroll.scrollWidth > scroll.clientWidth
+
+    if (!isVerticallyScrollable && !isHorizontallyScrollable) {
+      return
+    }
+
+    const isNavigatingBackward = currentIndex < previousIndexRef.current
+    const orientation = getOrientation()
+    previousIndexRef.current = currentIndex
+
+    if (isNavigatingBackward) {
+      if (orientation === 'vertical') {
+        scroll.scrollTo({ top: scroll.scrollHeight - scroll.clientHeight, left: 0, behavior: 'instant' })
+      } else if (orientation === 'vertical-reverse') {
+        scroll.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+      } else if (orientation === 'horizontal-reverse') {
+        scroll.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+      } else {
+        scroll.scrollTo({ top: 0, left: scroll.scrollWidth - scroll.clientWidth, behavior: 'instant' })
+      }
+    } else {
+      if (orientation === 'vertical-reverse') {
+        scroll.scrollTo({ top: scroll.scrollHeight - scroll.clientHeight, left: 0, behavior: 'instant' })
+      } else if (orientation === 'horizontal-reverse') {
+        scroll.scrollTo({ top: 0, left: scroll.scrollWidth - scroll.clientWidth, behavior: 'instant' })
+      } else {
+        scroll.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+      }
+    }
+  }, [currentIndex, getOrientation, scrollRef])
+}
