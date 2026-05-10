f9d8d87413d6014807a392aa0bc0021fe9d0c59d
litomi2026 <litomi2026@gmail.com>
2026-05-10 22:42:09 +0800

    fix: 전체화면 지원 여부

--- src/app/manga/[id]/ImageViewer/FullscreenButton.tsx
+++ src/app/manga/[id]/ImageViewer/FullscreenButton.tsx
@@ -1,27 +1,39 @@
 'use client'
 
 import { Maximize } from 'lucide-react'
-import { ComponentProps, memo } from 'react'
+import { type ComponentProps, useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
-export default memo(FullscreenButton)
+export default function FullscreenButton(props: ComponentProps<'button'>) {
+  const [isFullscreenSupported, setIsFullscreenSupported] = useState(false)
 
-function FullscreenButton(props: ComponentProps<'button'>) {
   function toggleFullScreen() {
     if (!document.fullscreenElement) {
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen().catch(() => toast.warning('전체화면 전환에 실패했어요'))
-      } else {
-        toast.warning('이 브라우저는 전체화면 기능을 지원하지 않아요')
-      }
+      document.documentElement.requestFullscreen().catch(() => toast.warning('전체화면 전환에 실패했어요'))
     } else if (document.exitFullscreen) {
       document.exitFullscreen().catch(() => toast.warning('전체화면 종료에 실패했어요'))
     }
+  }
+
+  useEffect(() => {
+    setIsFullscreenSupported(getIsFullscreenSupported())
+  }, [])
+
+  if (!isFullscreenSupported) {
+    return null
   }
 
   return (
     <button aria-label="전체화면" onClick={toggleFullScreen} {...props}>
       <Maximize className="size-6" />
     </button>
+  )
+}
+
+function getIsFullscreenSupported() {
+  return (
+    document.fullscreenEnabled &&
+    typeof document.documentElement.requestFullscreen === 'function' &&
+    typeof document.exitFullscreen === 'function'
   )
 }
