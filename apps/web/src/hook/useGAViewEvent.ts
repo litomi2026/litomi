1d444df0a834d0101769f612b28bb1ea8f98695d
litomi2026 <litomi2026@gmail.com>
2026-05-16 21:45:05 +0800

    fix: promotion 삭제

--- apps/web/src/hook/useGAViewEvent.ts
+++ apps/web/src/hook/useGAViewEvent.ts
@@ -4,6 +4,8 @@ import { useInView } from 'react-intersection-observer'
 
 import { track } from '@/lib/analytics/browser'
 
+import { useLatestRef } from './useLatestRef'
+
 type Options = {
   eventName: string
   eventParams?: Record<string, string>
@@ -11,18 +13,19 @@ type Options = {
 
 export default function useGAViewEvent({ eventName, eventParams }: Options) {
   const isViewed = useRef(false)
+  const eventParamsRef = useLatestRef(eventParams)
   const { ref, inView } = useInView({ threshold: 0.5 })
 
   useEffect(() => {
     if (inView && !isViewed.current) {
       const timer = setTimeout(() => {
         isViewed.current = true
-        track(eventName, eventParams)
+        track(eventName, eventParamsRef.current)
       }, ms('3 seconds'))
 
       return () => clearTimeout(timer)
     }
-  }, [eventName, eventParams, inView])
+  }, [eventName, eventParamsRef, inView])
 
   return { ref }
 }
