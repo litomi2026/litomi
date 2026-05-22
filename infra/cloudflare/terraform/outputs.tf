1c1de74c9f268183411c6bbb23db2270e939ec22
litomi2026 <litomi2026@gmail.com>
2026-05-22 09:06:15 +0800

    fix: litomi -> litomi-ops

--- infra/cloudflare/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cache_rules_count" {
-  description = "Number of cache rules configured"
-  value       = length(cloudflare_ruleset.cache_rules.rules)
-}
-
-output "rate_limiting_rules_count" {
-  description = "Number of rate limiting rules configured"
-  value       = length(cloudflare_ruleset.rate_limiting.rules)
-}
