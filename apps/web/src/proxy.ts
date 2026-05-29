6584198a5ad98dc0e7e8a26dc36e310f3880abea
litomi2026 <litomi2026@gmail.com>
2026-05-30 00:51:51 +0800

    Update proxy.ts

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,9 +1,3 @@
-import { applyCookieConfigs, getAuthCookieClearConfigs } from '@litomi/auth/cookie'
-import { JWTType, verifyJWT } from '@litomi/auth/jwt'
-import { buildSessionDeviceLabel } from '@litomi/auth/session'
-import { refreshSession } from '@litomi/auth/session/persistent-session'
-import { CookieKey } from '@litomi/http/cookie'
-import { getRequestUserAgent } from '@litomi/http/request'
 import { getPathLengthBlockStatus } from '@litomi/std'
 import { NextRequest, NextResponse } from 'next/server'
 
@@ -24,46 +18,12 @@ export const config = {
   ],
 }
 
-export async function proxy({ cookies, headers, nextUrl }: NextRequest) {
+export function proxy({ nextUrl }: NextRequest) {
   const pathLengthBlockStatus = getPathLengthBlockStatus(nextUrl.pathname)
 
   if (pathLengthBlockStatus) {
     return new NextResponse(null, { status: pathLengthBlockStatus })
   }
 
-  const accessToken = cookies.get(CookieKey.ACCESS_TOKEN)?.value
-  const refreshToken = cookies.get(CookieKey.REFRESH_TOKEN)?.value
-
-  if (!accessToken && !refreshToken) {
-    return NextResponse.next()
-  }
-
-  const validAccessToken = accessToken ? await verifyJWT(accessToken, JWTType.ACCESS).catch(() => null) : null
-
-  if (validAccessToken) {
-    return NextResponse.next()
-  }
-
-  if (!refreshToken) {
-    const response = NextResponse.next()
-    applyCookieConfigs(response.cookies, getAuthCookieClearConfigs())
-    return response
-  }
-
-  const deviceLabel = buildSessionDeviceLabel(getRequestUserAgent(headers))
-  const refreshResult = await refreshSession(refreshToken, deviceLabel)
-
-  if (!refreshResult.ok) {
-    const response = NextResponse.next()
-    applyCookieConfigs(response.cookies, getAuthCookieClearConfigs())
-    return response
-  }
-
-  const response = NextResponse.next()
-
-  for (const cookie of refreshResult.cookies) {
-    response.cookies.set(cookie.key, cookie.value, cookie.options)
-  }
-
-  return response
+  return NextResponse.next()
 }
