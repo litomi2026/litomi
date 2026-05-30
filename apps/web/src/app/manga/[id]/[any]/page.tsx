b199b172109e1bdb39547e5a15f42363731ffdb0
litomi2026 <litomi2026@gmail.com>
2026-05-30 12:53:18 +0800

    chore: move

--- apps/web/src/app/manga/[id]/[any]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// NOTE: 하위 호환
-export default async function Page({ params }: PageProps<'/manga/[id]'>) {
-  const { id } = await params
-  redirect(`/manga/${id}`)
-}
