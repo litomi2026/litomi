5a84409e3b3c3bd3552595d3621e4955faac963c
litomi2026 <litomi2026@gmail.com>
2026-06-05 22:19:52 +0800

    fix: ssl

--- packages/db/drizzle.app.config.ts
+++ packages/db/drizzle.app.config.ts
@@ -2,15 +2,14 @@ import { env as cliEnv } from '@litomi/env/cli'
 import { env } from '@litomi/env/server.common'
 import { defineConfig } from 'drizzle-kit'
 
+import { postgresURLToDrizzleCredentials } from './drizzle.postgres'
+
 const { APP_POSTGRES_CERTIFICATE } = env
 const { APP_POSTGRES_URL_DIRECT } = cliEnv
 
 export default defineConfig({
   schema: 'src/app/schema.ts',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: APP_POSTGRES_URL_DIRECT,
-    ssl: APP_POSTGRES_CERTIFICATE ? { ca: APP_POSTGRES_CERTIFICATE, rejectUnauthorized: true } : 'prefer',
-  },
+  dbCredentials: postgresURLToDrizzleCredentials(APP_POSTGRES_URL_DIRECT, APP_POSTGRES_CERTIFICATE),
   strict: true,
 })
