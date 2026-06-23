32f0c59c50c25f661310d3e3493235cf8e9e0e84
litomi2026 <litomi2026@gmail.com>
2026-06-23 16:21:51 +0900

    Update topNavigationActionConfig.ts

--- apps/web/src/app/[locale]/(navigation)/(top-navigation)/topNavigationActionConfig.ts
+++ apps/web/src/app/[locale]/(navigation)/(top-navigation)/topNavigationActionConfig.ts
@@ -1,5 +1,4 @@
-export const LIVE_CAM_AD_URL =
-  'https://t.nettrck.store/c1/d012584b-d43a-4900-9811-bdeaca53b632?externalId={impressionId}&cv1={impressionId}&cv2={userId}&cv3={device}&cv4={creativeId}&cv5={campaignId}&cv6={language}&cv7=%SLAVA_KPSS%&cv8={browser}&cv9={siteId}&cv10={creativeName}'
+export const LIVE_CAM_AD_URL = 'https://en.hotzcam.com/girls/korean'
 
 export const TOR_LINKS = {
   braveBrowserTor: {
